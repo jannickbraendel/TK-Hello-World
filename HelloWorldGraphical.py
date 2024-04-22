2df3f15f1b64f9f0e6d1b47fb088d2dcc298639e
jannickbraendel <jannick2@braendel.de>
2024-04-22 11:06:16 +0200

    created grid structure and quit button

--- HelloWorldGraphical.py
+++ HelloWorldGraphical.py
@@ -5,9 +5,10 @@ from tkinter import *
 
 window = Tk()
 window.geometry('250x300')
+window.minsize(250,300)
 
-languageLabel = Label(window, text='Please select a language.', fg='#F6A800', font='Helvetica')
-languageLabel.grid(row=0, sticky='nsew')
+languageLabel = Label(window, text='Please select a language.', fg='#F6A800', font='Helvetica', justify='center')
+languageLabel.grid(row=0, sticky='nsew', padx=10, pady=10)
 
 
 # displays greeting in chosen language to the language Label
@@ -23,15 +24,18 @@ def greet(lang):
     languageLabel.config(text=greeting)
 
 
-#insert your code here
-dButton = Button(window, text='Deutsch', fg='black', command=partial(greet, 'Deutsch'))
-dButton.grid(column=0, row=1, sticky='nsew')
+# create one button for each language (same config)
+languages = ['Deutsch', 'English', 'French']
+for i in range(3):
+    button = Button(window, text=languages[i], fg='black', command=partial(greet, languages[i]), justify='center',
+                    font='Helvetica 12', borderwidth=3)
+    button.grid(column=0, row=i + 1, sticky='nsew', padx=10, pady=10)
 
-eButton = Button(window, text='English', fg='black', command=partial(greet, 'English'))
-eButton.grid(column=0, row=2, sticky='nsew')
+quitButton = Button(window, text='Quit', fg='black', command=window.quit, justify='center', font='Helvetica 10')
+quitButton.grid(column=0, row=4, sticky='nse', padx=(70, 10), pady=(50, 10))
 
-fButton = Button(window, text='French', fg='black', command=partial(greet, 'French'))
-fButton.grid(column=0, row=3, sticky='nsew')
-
+# columns and rows are adjusted when window size is changed
 window.columnconfigure(0, weight=1)
+for i in range(5):
+    window.rowconfigure(i, weight=1)
 window.mainloop()
